a10c781aaf5ad512c91a12ec851073c043617f3c
rianhidayat <ryanhidayat.rh197@gmail.com>
2023-01-24 20:58:15 +0800

    try CRUD

--- src/materi/todo/components/UserForm.jsx
+++ src/materi/todo/components/UserForm.jsx
@@ -10,11 +10,11 @@ const UserForm = (props) => {
     <Form onSubmit={handleSubmit}>
       <Form.Group className='mb-3'>
         <Form.Label>Email address</Form.Label>
-        <Form.Control name='name' type='text' placeholder='Enter name' onChange={() => handleChange} value={formData.name} />
+        <Form.Control name='name' type='text' placeholder='Enter name' onChange={handleChange} value={formData.name} />
       </Form.Group>
       <Form.Group className='mb-3'>
         <Form.Label>Username</Form.Label>
-        <Form.Control name='username' type='text' placeholder='Enter username' onChange={() => handleChange} value={formData.username} />
+        <Form.Control name='username' type='text' placeholder='Enter username' onChange={handleChange} value={formData.username} />
       </Form.Group>
 
       <Button variant='primary' type='submit'>
